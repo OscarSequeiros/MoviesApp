839fbe133bb0340829e4a8b90b0c8738c038e31e
Oscar Sequeiros serrano <oscar.sequeiros_ex@scotiabank.cl>
2021-01-24 13:49:00 -0300

    Using ViewBinding in MoviesFragment.

--- app/src/main/java/com/example/moviesapp/presentation/ui/MoviesFragment.kt
+++ app/src/main/java/com/example/moviesapp/presentation/ui/MoviesFragment.kt
@@ -5,19 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.moviesapp.databinding.FragmentMoviesBinding
 
 class MoviesFragment : Fragment() {
+
+    private var binding: FragmentMoviesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentMoviesBinding.inflate(inflater)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
