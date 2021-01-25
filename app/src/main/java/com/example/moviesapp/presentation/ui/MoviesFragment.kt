99a2afdcfd07cf63ce67ba53630d27d19aada937
Oscar Sequeiros serrano <oscar.sequeiros_ex@scotiabank.cl>
2021-01-24 22:15:43 -0300

    Marking @AndroidEntryPoint

--- app/src/main/java/com/example/moviesapp/presentation/ui/MoviesFragment.kt
+++ app/src/main/java/com/example/moviesapp/presentation/ui/MoviesFragment.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.moviesapp.databinding.FragmentMoviesBinding
+import com.example.moviesapp.presentation.MoviesState
+import com.example.moviesapp.presentation.MoviesState.*
+import com.example.moviesapp.presentation.viewmodel.MoviesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MoviesFragment : Fragment() {
 
     private var binding: FragmentMoviesBinding? = null
+
+    private val viewModel: MoviesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +35,23 @@ class MoviesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        /*viewModel.stateFlow.observeOn()
+            //.onEach { state -> render(state) }
+            .collect { state -> render(state) }
+            .launchIn(lifecycleScope)*/
+        lifecycleScope.launch {
+            viewModel.stateFlow
+                .collect { state -> render(state) }
+        }
+    }
+
+    private fun render(state: MoviesState) {
+        when(state) {
+            is IdleState -> Toast.makeText(context, "idle", Toast.LENGTH_SHORT).show()
+            is LoadingState -> Toast.makeText(context, "loading", Toast.LENGTH_SHORT).show()
+            is EmptyMoviesState -> Toast.makeText(context, "empty", Toast.LENGTH_SHORT).show()
+            is FilledMoviesState -> Toast.makeText(context, "filled", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
