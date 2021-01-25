bebfc8ef23660accd807640e21a9323a51bb4a34
Oscar Sequeiros serrano <oscar.sequeiros_ex@scotiabank.cl>
2021-01-24 23:58:18 -0300

    Implementing MoviesAdapter

--- app/src/main/java/com/example/moviesapp/presentation/ui/MoviesAdapter.kt
+++ app/src/main/java/com/example/moviesapp/presentation/ui/MoviesAdapter.kt
@@ -1,11 +1,35 @@
 package com.example.moviesapp.presentation.ui
 
+import android.view.LayoutInflater.from
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviesapp.databinding.ItemMovieBinding
 import com.example.moviesapp.presentation.model.PresentationMovie
 import javax.inject.Inject
 
 class MoviesAdapter @Inject constructor(
-    private val clickAction: (movieId: Long) -> Unit
-) {
+    private val clickListener: (movieId: Long) -> Unit
+) : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
 
     private var movies: List<PresentationMovie> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = ItemMovieBinding.inflate(from(parent.context), parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    inner class MovieViewHolder(private val binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(movie: PresentationMovie) = with(binding) {
+            textTitle.text = movie.title
+            cardContainer.setOnClickListener { clickListener.invoke(movie.id) }
+        }
+    }
 }
